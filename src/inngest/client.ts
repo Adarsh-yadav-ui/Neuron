225972e209f1af1385806e722562623103eb2393
Adarsh Yadav <akankshaazm20@gmail.com>
2026-05-19 13:58:23 +0530

    09:Inngest fixed

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,6 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "neuron" });
+export const inngest = new Inngest({
+  id: "neuron",
+  signingKey: process.env.INNGEST_SIGNING_KEY,
+});
